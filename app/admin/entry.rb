90bb9e949a9b09e0349beb91c015617bd0329659
hackvier <hackvier@localhost.localdomain>
2015-07-02 22:38:14 -0300

    try nested dinamic entry and  bill forms new

--- app/admin/entry.rb
+++ app/admin/entry.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Entry do
 menu :priority => 3
- filter :turn
+filter :turn
 permit_params :bill, :store_id, :turn
 
 scope :all, :default => true
@@ -31,16 +31,33 @@ scope :all, :default => true
     column :store_id
     column :turn
     column :initial_value
-    column :bill
-    column :quantity
+    #column :bill
+    #column :quantity
     column :posnet
     column :date
     column :total_general
     column :updated_at
     actions
-    
 end
 
+form do |f|
+      f.inputs "Entry" do 
+        f.input :store_id
+        f.input :turn
+        f.input :initial_value
+        f.input :posnet
+        f.input :total_general
+        f.input :updated_at
+      end
+
+  f.has_many :bills do |bill|
+      
+        bill.input :quantity
+        bill.input :total	
+      end
+
+      f.actions
 
 
+end
 end
