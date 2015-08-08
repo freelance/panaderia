9c92510f5c909eabc2ecdc14e1a776afccf87a1a
hackvier <hackvier@localhost.localdomain>
2015-08-08 02:18:05 -0300

    models for order and product and client

---
+++ db/migrate/20150808045901_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.integer :user_id
+      t.integer :client_id
+      t.datetime :checked_out_at
+      t.decimal :total_price
+      t.integer :store_id
+
+      t.timestamps null: false
+    end
+  end
+end
